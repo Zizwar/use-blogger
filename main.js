b527e7038645baf73457dd82dfaf8a8e45913b95
Zizwar <zizwar@gmail.com>
2023-02-15 03:16:11 +0000

    fix callback get resault without async

--- main.js
+++ main.js
@@ -1,27 +1,37 @@
 window["cbdata"] = (data) => {
-  alert(2323);
-  console.log("olalal=", data);
+  //alert(99);
+  console.log("-data load callback");
+  if (window["$callback"]) window["$callback"](data);
 };
 const URL_BLOGGER_BROWSER = ({ blogId, categorie, callback }) =>
   `https://www.blogger.com/feeds/${blogId}/posts/default${
     categorie ? "/-/" + categorie : ""
   }/?${callback ? "callback=" + callback : ""}&alt=json`;
 
-export class QueryGBlogger {
+export default class QueryGBlogger {
   constructor({ blogId, isBrowser }) {
     this.blogId = blogId;
     this.isBrowser = isBrowser;
-    console.log('start qgb')
+    console.log("start qgb");
   }
-  get data() {
+  load({ callback }) {
+    window["$callback"] = callback;
     // if (this.isBrowser) {
     const url = URL_BLOGGER_BROWSER({
       blogId: this.blogId,
-      callback: this.callback ? "cbdata" : undefined,
+      callback: this.isBrowser ? "cbdata" : undefined,
     });
-    return url;
+
     let script = document.createElement("script");
     script.src = url;
+    script.async = true;
+
+    script.onerror = () => {
+      console.error("Error occurred while loading feed blogger");
+    };
+    script.onload = () => {
+      console.log("load script feed :)");
+    };
     document.body.append(script);
   }
 

fed7daa6443dd570f955b6cd49a70a56b8b865c8
Ibrahim BIDI <Zizwar@users.noreply.github.com>
2023-03-02 04:16:25 +0100

    Update index.d.ts

--- @types/wino-blogger/index.d.ts
+++ @types/wino-blogger/index.d.ts
@@ -1,42 +1,56 @@
-declare module "wino-blogger" {
-    export const regexs: {
-      https: string;
-      dictionary: string;
-      tuple: string;
-      array: string;
-      src: string;
-      video: string;
-      custom: (r: any) => any;
-    };
-    export function regexIno(content: string, pattern?: RegExp): string[];
-    export function urlJsonSearchPostsCategories(options: {
-      category?: string;
-      postId?: string;
-      query?: string;
-      blogUrl?: string;
-      blogId?: string;
-    }): string;
-    export default class WinoBlogger {
-      constructor(props?: {
-        blogId?: string;
-        isBrowser?: boolean;
-        saveTmp?: boolean;
-        blogUrl?: string;
-      });
-      categories(cats?: string[]): this;
-      labels(cats?: string[]): this;
-      post(postId?: string): this;
-      search(text?: string): this;
-      limit(n?: number): this;
-      skip(n: number): this;
-      orderby(value?: string): this;
-      callback(cb: (data: any) => void): this;
-      setData(data: any): this;
-      getData(): any;
-      published(dateMin?: string, dateMax?: string): this;
-      updated(dateMin?: string, dateMax?: string): this;
-      load(variables: any[]): Promise<any>;
-      exec(variables: any[]): Promise<any>;
-    }
-  }
-  
+declare function regexIno(content: string, pattern?: RegExp): string[];
+
+declare function urlJsonSearchPostsCategories(params: {
+  category?: string;
+  postId?: string;
+  query?: string;
+  blogUrl?: string;
+  blogId?: string;
+}): string;
+
+declare class WinoBlogger {
+  blogUrl: string;
+  blogId: string;
+  saveTmp: any;
+  isBrowser: boolean;
+  data: any;
+  category: string;
+  postId: string;
+  query: string;
+  variables: any[];
+  constructor(props?: {
+    blogId?: string;
+    isBrowser?: boolean;
+    saveTmp?: any;
+    blogUrl?: string;
+  });
+  categories(cats?: string[]): this;
+  labels(cats?: string[]): this;
+  post(postId?: string): this;
+  search(text?: string): this;
+  limit(n?: number): this;
+  skip(n?: number): this;
+  orderby(value?: string): this;
+  callback(cb: any): this;
+  setData(data: any): this;
+  getData(): any;
+  published(dateMin?: string, dateMax?: string): this;
+  updated(dateMin?: string, dateMax?: string): this;
+  load(variables: any[]): Promise<any>;
+  exec(variables: any[]): Promise<any>;
+}
+
+declare function getPost(
+  entry: any,
+  variables?: any[]
+): {
+  id: string;
+  content: string;
+  media$thumbnail: any;
+  published: string;
+  updated: string;
+  title: string;
+  category: any;
+  link: any;
+};
+

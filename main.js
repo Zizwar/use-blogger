d7f286d0be8edaf03b11a50e0dcfa294155209ec
Ibrahim BIDI <Zizwar@users.noreply.github.com>
2023-03-09 04:40:07 +0100

    switch to typescript

--- main.js
+++ main.js
@@ -1,38 +1,30 @@
-function regexIno(content, pattern = new RegExp(regexs.dictionary, "g")) {
-  let match;
-  const matchArr = [];
-  while ((match = pattern.exec(content))) {
-    match = match[1]?.trim();
-    if (match) matchArr.push(match);
-  }
-  return matchArr;
-}
-//get all post each categories
-function urlJsonSearchPostsCategories({
-  category = "",
-  postId = "",
-  query = "",
-  blogUrl,
-  blogId,
-}) {
-  return `${
-    blogUrl || "https://www.blogger.com/" + blogId
-  }/feeds/posts/default/${postId}${
-    category ? `-/${category}` : ""
-  }?alt=json&${query}`;
-}
-//fet llop
+export const matcher = {
+  https: 'https(.*?)"',
+  dictionary: "{(.*?)}",
+  tuple: "((.*?))",
+  array: "[(.*?)]",
+  src: ' src="(.*?)" ',
+  video: "<iframe*(.*?) src='*(.*?)' ",
+  custom: (_r: string) => _r,
+};
+
 export default class WinoBlogger {
-  blogUrl; // if blogUrl not req blogId
-  blogId; //if blogId not req blogeUrl
-  saveTmp;
-  isBrowser;
-  data;
-  category = "";
-  postId = "";
-  query = "";
-  variables = [];
-  constructor(props) {
+  blogUrl: string; // if blogUrl not req blogId
+  blogId: string; //if blogId not req blogeUrl
+  saveTmp: any;
+  isBrowser: boolean;
+  data: any;
+  category: string = "";
+  postId: string = "";
+  query: string = "";
+  variables: any[] = [];
+
+  constructor(props: {
+    blogId?: string;
+    isBrowser: boolean;
+    saveTmp: any;
+    blogUrl?: string;
+  }) {
     const { blogId = "", isBrowser, saveTmp, blogUrl = "" } = props || [];
     this.blogId = blogId;
     this.isBrowser = isBrowser;
@@ -40,60 +32,71 @@ export default class WinoBlogger {
     this.blogUrl = blogUrl;
   }
 
-  categories(cats = []) {
+  categories(cats: string[] = []): WinoBlogger {
     this.category = cats?.join("/") || "";
     return this;
   }
-  labels(cats = []) {
+
+  labels(cats: string[] = []): WinoBlogger {
     this.category = cats?.join("/") || "";
     return this;
   }
-  post(postId = "") {
+
+  post(postId: string = ""): WinoBlogger {
     this.postId = postId;
     return this;
   }
+
   //fn query
-  search(text = "") {
+  search(text: string = ""): WinoBlogger {
     this.query += `q=${text}&`;
     return this;
   }
-  limit(n = 3) {
+
+  limit(n: number = 3): WinoBlogger {
     this.query += `max-results=${n}&`;
     return this;
   }
-  skip(n) {
+
+  skip(n: number): WinoBlogger {
     if (n) this.query += `start-index=${n}&`;
     return this;
   }
-  orderby(value = "published") {
+
+  orderby(value: string = "published"): WinoBlogger {
     //or updated
     this.query += `orderby=${value}&`;
     return this;
   }
+
   //
-  callback(cb) {
+  callback(cb: any): WinoBlogger {
     this.callback = cb;
     return this;
   }
-  setData(data) {
+
+  setData(data: any): WinoBlogger {
     this.data = data;
     return this;
   }
-  getData() {
+
+  getData(): any {
     return this.data;
   }
 
-  published(dateMin, dateMax) {
+  published(dateMin: string, dateMax: string): WinoBlogger {
     if (dateMin) this.query += `published-min=${dateMin}&`;
     if (dateMax) this.query += `published-max=${dateMax}&`;
     return this;
   }
-  updated(dateMin, dateMax) {
+
+  updated(dateMin: string, dateMax: string): WinoBlogger {
     if (dateMin) this.query += `updated-min=${dateMin}&`;
     if (dateMax) this.query += `updated-max=${dateMax}&`;
     return this;
   }
-  async load(variables) {
+
+  async load(variables: any[]): Promise<any> {
     try {
       const { category, postId, query, blogUrl, blogId } = this;
       if (!this.data) {
@@ -118,48 +121,96 @@ export default class WinoBlogger {
       console.error("There was a problem with the fetch request:", error);
     }
   }
+
   //clone load
-  async exec(variables) {
+  async exec(variables: any) {
     return await this.load(variables);
   }
 }
+//
+
+interface Variable {
+  key: string;
+  type?: "string" | "number" | "array" | "full";
+  regex?: string;
+}
+
+//
+
+interface Category {
+  term: string;
+}
+interface Data {
+  id: string;
+  name: string;
+  thumbnail: string | undefined;
+  published: string;
+  videos: string;
+  link: {
+    [key: string]: string;
+  };
+  images: string[];
+  text: string;
+  html: string;
+  categories: string[];
+  category: string;
+  updated: string;
+  [key: string]: any;
+}
+interface Entry {
+  id: {
+    $t: string;
+  };
+  content: {
+    $t: string;
+  };
+  media$thumbnail?: {
+    url: string;
+  };
+  published: {
+    $t: string;
+  };
+  updated: {
+    $t: string;
+  };
+  title: {
+    $t: string;
+  };
+  category?: Category[];
+  link: {
+    [key: string]: string;
+  };
+}
+//
 function getPost(
   {
     id: { $t: _id },
     content: { $t: _content },
-    media$thumbnail, //: thumbnail,//{ url: thumbnail },
+    media$thumbnail,
     published: { $t: published },
     updated: { $t: updated },
     title: { $t: name },
     category,
     link,
-  },
-  variables = []
+  }: Entry,
+  variables: Variable[] = []
 ) {
-  const regexs = {
-    https: 'https(.*?)"',
-    dictionary: "{(.*?)}",
-    tuple: "((.*?))",
-    array: "[(.*?)]",
-    src: ' src="(.*?)" ',
-    video: "<iframe*(.*?) src='*(.*?)' ",
-    custom: (_r) => _r,
-  };
   _content = _content.replace(/&nbsp;/gi, "");
-  //get videos array
-  const _videos = new RegExp(regexs.video, "g").exec(_content) || [];
-  const videos = _videos[2] || "";
-  //get image
-  const images =
-    regexIno(_content, new RegExp(regexs.src, "g"))?.map((img = "") => img) ||
+
+  const _videos = new RegExp(matcher.video, "g").exec(_content) || [];
+  const videos: string = _videos[2] || "";
+
+  const images: string[] =
+    regexIno(_content, new RegExp(matcher.src, "g"))?.map((img = "") => img) ||
     [];
-  const content = _content.replace(/(<([^>]+)>)/gi, "");
-  function getVar({ key, type = "string", regex }) {
+
+  function getVar({ key, type = "string", regex }: Variable) {
     let _res =
       regexIno(_content, new RegExp(regex || `${key}*[:=]*(.*?)[;<]`, "g")) ||
       [];
 
     if (type === "full") return _res;
+
     let res = _res[0];
     if (type === "number")
       return res?.match(/\d+(\.\d+)?/g)?.map((_r) => +_r)[0] || 0;
@@ -167,16 +218,17 @@ function getPost(
     return res;
   }
 
-  const vars = [];
+  const vars: { [key: string]: any } = {};
   variables &&
-    variables.forEach(({ key, type, regex }) => {
+    variables.forEach(({ key, type, regex }: Variable) => {
       vars[key] = getVar({ key, type, regex });
     });
-  ///
-  const categories = category?.map((cat) => cat.term) || [];
-  const thumbnail = media$thumbnail?.url;
-  const id = _id.split("post-")[1];
-  const data = {
+
+  const categories: string[] = category?.map((cat) => cat.term) || [];
+  const thumbnail: string | undefined = media$thumbnail?.url;
+  const id: string = _id.split("post-")[1];
+
+  const data: Data = {
     id,
     name,
     thumbnail,
@@ -184,23 +236,59 @@ function getPost(
     videos,
     link,
     images,
-    content,
-    contentHTML: _content,
+    text: _content.replace(/(<([^>]+)>)/gi, ""),
+    html: _content,
     categories,
     category: categories[0],
     updated,
     ...vars,
+    content: "",
+    contentHTML: "",
   };
   if (categories?.includes("$")) return { $: data };
   return { data };
 }
-function getPosts(dataPosts = [], variables) {
-  const posts = [];
-  const fnk = [];
-  dataPosts.feed?.entry?.forEach((entry) => {
+
+function getPosts(dataPosts: any, variables: any) {
+  const posts: any[] = [];
+  const fnk: any[] = [];
+  dataPosts.feed?.entry?.forEach((entry: any) => {
     const post = getPost(entry, variables);
     if (post.$) fnk.push(post.$);
     else posts.push(post.data);
   });
   return { data: posts, $: fnk };
+}
+
+function regexIno(
+  content: string,
+  pattern: RegExp = new RegExp(matcher.dictionary, "g")
+): string[] {
+  let match;
+  const matchArr = [];
+  while ((match = pattern.exec(content))) {
+    match = match[1]?.trim();
+    if (match) matchArr.push(match);
+  }
+  return matchArr;
+}
+
+function urlJsonSearchPostsCategories({
+  category = "",
+  postId = "",
+  query = "",
+  blogUrl,
+  blogId,
+}: {
+  category?: string;
+  postId?: string;
+  query?: string;
+  blogUrl?: string;
+  blogId?: string;
+}): string {
+  return `${
+    blogUrl || "https://www.blogger.com/" + blogId
+  }/feeds/posts/default/${postId}${
+    category ? `-/${category}` : ""
+  }?alt=json&${query}`;
 }
